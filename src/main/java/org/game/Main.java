3bb4e1e48c175ea656973eb3a354e78020ebe9c7
Alan Chang <alannnchang@gmail.com>
2025-03-09 22:10:39 -0700

    Initial Commit

---
+++ src/main/java/org/game/Main.java
@@ -0,0 +1,14 @@
+package org.game;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Will you find the secret code?");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+
+        }
+    }
+}
