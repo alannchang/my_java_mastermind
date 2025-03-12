ae41ed65f36ef5ebba1052f7be2d913adb3119bc
Alan Chang <alannnchang@gmail.com>
2025-03-11 22:26:21 -0700

    added separator, number of rounds, game ends at round 10

--- src/main/java/org/game/GameLoop.java
+++ src/main/java/org/game/GameLoop.java
@@ -1,10 +1,12 @@
 package org.game;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class GameLoop {
     int[] secretCode = new int[4];
     Random rand = new Random();
+    int round = 0;
 
     public GameLoop(String userProvidedSecret){
         if (userProvidedSecret == null) {
@@ -14,10 +16,23 @@ public class GameLoop {
                 secretCode[i] = Character.getNumericValue(userProvidedSecret.charAt(i));
             }
         }
-        System.out.println("The secret code is ");
-        for (int i = 0; i < secretCode.length; i++) {
-            System.out.print(secretCode[i]);
+        Scanner scn = new Scanner(System.in);
+
+        while(round < 10){
+            System.out.printf("---\nRound %d\n>", round);
+            String playerGuess = scn.nextLine();
+            System.out.println("Well placed pieces: ");
+            System.out.println("Misplaced pieces: ");
+            round++;
         }
+        if (round > 9) {
+            System.out.println("The secret code is ");
+            for (int j : secretCode) {
+                System.out.print(j);
+            }
+            System.out.print("\nYou lose! Try again!\n");
+        }
+
     }
 
     void generateSecretCode() {
