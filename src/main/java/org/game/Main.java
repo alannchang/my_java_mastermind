4cdd58105ca322f947a244ce8d18df54afaf65cf
Alan Chang <alannnchang@gmail.com>
2025-03-15 12:34:48 -0700

    Game will exit with status 1 if invalid code passed as argument

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -8,10 +8,14 @@ public class Main {
         if (args.length == 2 && args[0].equals("-c")) {
             if (isValidCode(args[1])) {
                 secretCode = args[1];
+            } else {
+                System.err.println("Invalid code, please input a 4 digit code.");
+                System.exit(1);
             }
         }
 
         System.out.println("Will you find the secret code?");
         GameLoop gameLoop = new GameLoop(secretCode);
+        System.exit(0);
     }
 }
